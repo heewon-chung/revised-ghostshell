f83cff6cfbcf577f3975b684c41d6a03d76c4a3e
Heewon Chung <runghyun@snu.ac.kr>
2017-07-06 18:53:12 +0900

    implement algorithm for ZZ to ZZX

--- include/generalTools.h
+++ include/generalTools.h
@@ -6,11 +6,13 @@
 #include <vector>
 
 #include "NTL/ZZ.h"
+#include "NTL/ZZX.h"
+#include "constant.h"
 
 using namespace std;
 using namespace NTL;
 
-void generateVectorInstance(vector<vector<long>>&, const long, const long);
-
+void integerToZZX(ZZX&, const ZZ&);
+void generateVectorInstance(vector<ZZX>&);
 
 #endif
--- src/generalTools.cpp
+++ src/generalTools.cpp
@@ -1,10 +1,29 @@
 #include "generalTools.h"
 
-void generateVectorInstance(vector<vector<long>>& vec, const long numBits, const long numSlots){
-    vec.clear();
-    vec.resize(numBits);
 
-    for(unsigned long i = 0; i < numBits; i++){
-        vec[i].resize(numSlots, rand() % 2 + 1);
+void integerToZZX(ZZX& encodedPoly, const ZZ& msg){
+    ZZ      tmpMsg = msg;
+    long    amt = 0;
+
+    while(tmpMsg > 0){
+        long rem = tmpMsg % PRIMEFIELD;
+        SetCoeff(encodedPoly, amt, rem);
+        amt++;
+        tmpMsg -= rem;
+        tmpMsg /= PRIMEFIELD;
+    }
+}
+
+void generateVectorInstance(vector<ZZX>& vecPoly){
+    vecPoly.clear();
+    vecPoly.resize(NUMBITS);
+    
+    for(unsigned long i = 0; i < NUMBITS; i++){
+        if(rand() % 2){
+            integerToZZX(vecPoly[i], MSGRND1);
+        }
+        else{
+            integerToZZX(vecPoly[i], MSGRND1 + 1);
+        }
     }
 }
