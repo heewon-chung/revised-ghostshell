62d17130f2443320738453d7bc980a98165e3050
Heewon Chung <runghyun@snu.ac.kr>
2017-08-11 18:36:26 +0900

    change random bits

--- include/constant.h
+++ include/constant.h
@@ -14,7 +14,7 @@ const long          C               = 3;
 const unsigned long NUMBITS         = 400;
 const unsigned long THRESHOLD       = 100;
 
-const unsigned long RANDOMBIT       = 40;
+const unsigned long RANDOMBIT       = 80;
 const unsigned long GENERATORBIT    = 30;
 const unsigned long DLGROUPORDER    = 100;
 
