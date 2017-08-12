b09d319bcb9e1a07e6bf211d54831214664dd11d
Heewon Chung <runghyun@snu.ac.kr>
2017-08-12 16:51:07 +0900

    minor change: change the variable name

--- src/match.cpp
+++ src/match.cpp
@@ -17,14 +17,14 @@ long hammingDistance(const vector<ZZX>& msg1, const vector<ZZX>& msg2){
 
 void hammingDistance(Ctxt& hdCtxt, const vector<Ctxt>& ct1, const vector<Ctxt>& ct2){
     assert(ct1.size() == ct2.size());
-    unsigned long numBits = ct1.size();
+    unsigned long ctxtSize = ct1.size();
 
     hdCtxt = ct1[0];
     hdCtxt -= ct2[0];
     hdCtxt.square();
 
     #pragma omp parallel for
-    for(unsigned long i = 1; i < numBits; i++){
+    for(unsigned long i = 1; i < ctxtSize; i++){
         Ctxt tmpCtxt = ct1[i];
         tmpCtxt -= ct2[i];
         tmpCtxt.square();
